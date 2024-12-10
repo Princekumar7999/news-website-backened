f2fd9e4f7af939770b7160f207ab6d2ea8130305
Prince kumar <142075834+Princekumar7999@users.noreply.github.com>
2024-12-10 13:58:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "475c0c34c7fd41b08a447c39451e2704";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
